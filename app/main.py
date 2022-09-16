66fdce354bfe3cbee0c052809e772f3ad0426538
Pierre-Louis Sergent <papa.louis59@gmail.com>
2022-09-16 17:06:40 +0200

    Add global exception

--- app/main.py
+++ app/main.py
@@ -54,31 +54,34 @@ def profile(request: Request, id: str):
     except:
         user = mlpyvrs.get_user_by_id(id)
 
-    if ("code" in user.profileData and user.get_code() == 404) or "stat_trackers" not in user.profileData['server_data']:
-        return templates.TemplateResponse("404.html", {"request": request, "title": "No data found", "message": "No data found for this user."})
+    try:
+        if ("code" in user.profileData and user.get_code() == 404) or "stat_trackers" not in user.profileData['server_data']:
+            return templates.TemplateResponse("404.html", {"request": request, "title": "No data found", "message": "No data found for this user."})
 
-    total_win = user.get_match_won_count(GamemodeMatches.OneVsOne)+user.get_match_won_count(GamemodeMatches.TwoVsTwo)
-    total_loss = user.get_match_lost_count(GamemodeMatches.OneVsOne)+user.get_match_lost_count(GamemodeMatches.TwoVsTwo)
-    total_win_percentage = round(user.get_global_win_percentage(), 2)
+        total_win = user.get_match_won_count(GamemodeMatches.OneVsOne)+user.get_match_won_count(GamemodeMatches.TwoVsTwo)
+        total_loss = user.get_match_lost_count(GamemodeMatches.OneVsOne)+user.get_match_lost_count(GamemodeMatches.TwoVsTwo)
+        total_win_percentage = round(user.get_global_win_percentage(), 2)
 
-    # Top characters
-    top_characters_slug = user.get_top_character_wins()
-    top_characters = {}
-    for character_slug, wins in top_characters_slug.items():
-        character = get_character_from_slug(character_slug)
-        top_characters[character.value["name"]] = {
-            "wins": wins,
-            "OvO_MMR": round(user.get_character_rating(character, RatingKeys.Mean, GamemodeRating.OneVsOne), 0),
-            "TvT_MMR": round(user.get_character_rating(character, RatingKeys.Mean, GamemodeRating.TwoVsTwo), 0)
-        }
+        # Top characters
+        top_characters_slug = user.get_top_character_wins()
+        top_characters = {}
+        for character_slug, wins in top_characters_slug.items():
+            character = get_character_from_slug(character_slug)
+            top_characters[character.value["name"]] = {
+                "wins": wins,
+                "OvO_MMR": round(user.get_character_rating(character, RatingKeys.Mean, GamemodeRating.OneVsOne), 0),
+                "TvT_MMR": round(user.get_character_rating(character, RatingKeys.Mean, GamemodeRating.TwoVsTwo), 0)
+            }
 
-    # 1v1
-    OneVsOne_infos = get_user_info_for_gamemode(mlpyvrs, user, GamemodeRating.OneVsOne, GamemodeRank.OneVsOne)
+        # 1v1
+        OneVsOne_infos = get_user_info_for_gamemode(mlpyvrs, user, GamemodeRating.OneVsOne, GamemodeRank.OneVsOne)
 
-    # 2v2
-    TwoVsTwo_infos = get_user_info_for_gamemode(mlpyvrs, user, GamemodeRating.TwoVsTwo, GamemodeRank.TwoVsTwo)
+        # 2v2
+        TwoVsTwo_infos = get_user_info_for_gamemode(mlpyvrs, user, GamemodeRating.TwoVsTwo, GamemodeRank.TwoVsTwo)
 
-    username = OneVsOne_infos["username"]
+        username = OneVsOne_infos["username"]
+    except:
+        return templates.TemplateResponse("404.html", {"request": request, "title": "Error", "message": "Error while fetching data."})
         
     return templates.TemplateResponse("profile.html",
         {"request": request, "title": "Profile", "username": username, "user": user,
@@ -94,35 +97,37 @@ def live(request: Request, id: str):
     except:
         user = mlpyvrs.get_user_by_id(id)
     
-    for network in user.get_user_networks():
-        if network.get_network_name() == "wb_network":
-            username = network.get_network_user_username()
-    user_match_history = mlpyvrs.get_user_match_history(user)
-    last_match = user_match_history.get_last_match()
+    try:
+        for network in user.get_user_networks():
+            if network.get_network_name() == "wb_network":
+                username = network.get_network_user_username()
+        user_match_history = mlpyvrs.get_user_match_history(user)
+        last_match = user_match_history.get_last_match()
 
-    if last_match is None:
-        return templates.TemplateResponse("404.html", {"request": request, "title": "No data found", "message": "No match found for this user."})
+        if last_match is None:
+            return templates.TemplateResponse("404.html", {"request": request, "title": "No data found", "message": "No match found for this user."})
 
-    state = last_match.get_state()
-    if state == "open":
-        if len(last_match.rawData["players"]["current"]) == 1:
-            match_type = "1v1"
-            title = f"Waiting for a game to start..."
+        state = last_match.get_state()
+        if state == "open":
+            if len(last_match.rawData["players"]["current"]) == 1:
+                match_type = "1v1"
+                title = f"Waiting for a game to start..."
+            else:
+                match_type = "1v1" if len(last_match.rawData["players"]["current"]) == 2 else "2v2"
+                title = f"Live - {match_type}"
         else:
-            match_type = "1v1" if len(last_match.rawData["players"]["current"]) == 2 else "2v2"
-            title = f"Live - {match_type}"
-    else:
-        match_type = "1v1" if last_match.get_player_ammount_in_match() == 2 else "2v2"
-        title = f"Finished - {match_type}"
-    
-    players = []
-    gmrating = GamemodeRating.OneVsOne if match_type == "1v1" else GamemodeRating.TwoVsTwo
-    gmrank = GamemodeRank.OneVsOne if match_type == "1v1" else GamemodeRank.TwoVsTwo
-    if title.startswith("Live"):
-        for id in last_match.rawData["players"]["current"]:
-            players.append(get_user_info_for_gamemode(mlpyvrs, mlpyvrs.get_user_by_id(id), gmrating, gmrank))
-    elif title.startswith("Finished"):
-        for id in last_match.rawData["win"] + last_match.rawData["loss"]:
-            players.append(get_user_info_for_gamemode(mlpyvrs, mlpyvrs.get_user_by_id(id), gmrating, gmrank))
-    
+            match_type = "1v1" if last_match.get_player_ammount_in_match() == 2 else "2v2"
+            title = f"Finished - {match_type}"
+        
+        players = []
+        gmrating = GamemodeRating.OneVsOne if match_type == "1v1" else GamemodeRating.TwoVsTwo
+        gmrank = GamemodeRank.OneVsOne if match_type == "1v1" else GamemodeRank.TwoVsTwo
+        if title.startswith("Live"):
+            for id in last_match.rawData["players"]["current"]:
+                players.append(get_user_info_for_gamemode(mlpyvrs, mlpyvrs.get_user_by_id(id), gmrating, gmrank))
+        elif title.startswith("Finished"):
+            for id in last_match.rawData["win"] + last_match.rawData["loss"]:
+                players.append(get_user_info_for_gamemode(mlpyvrs, mlpyvrs.get_user_by_id(id), gmrating, gmrank))
+    except:
+        return templates.TemplateResponse("404.html", {"request": request, "title": "Error", "message": "Error while fetching data."})
     return templates.TemplateResponse("live.html", {"request": request, "title": title, "username": username, "players": players})
